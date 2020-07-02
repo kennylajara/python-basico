2cecba7b84c96c1bf0cb05bd26ccb8f35b4681dd
Kenny Lajara <kennylajara@gmail.com>
2020-07-02 18:00:01 -0400

    Borrar archivos viejos

--- conversor-de-monedas.py
+++
@@ -1,7 +0,0 @@
-# CONVERSOR DE MONEDAS
-moneda_local = int( input( "¿Cuánto dinero tienes en tu moneda local? " ))
-tasa_del_dia = int( input ( "¿Cuál es la tasa del día? " ))
-
-resultado = round( moneda_local / tasa_del_dia, 2)
-
-print ( f'Equivale a {resultado} en la moneda a que deseas convertir')
